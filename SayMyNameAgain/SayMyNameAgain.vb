52e3a2dc9fd839bec4e6694652106c449d51587a
Tusk <Tuskman045@gmail.com>
2025-01-29 08:19:13 -0700

    Add project files.

---
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -0,0 +1,23 @@
+﻿Module SayMyNameAgain
+
+    Sub Main()
+        Dim userInput As String
+
+        Console.WriteLine("What is your name?") 'Ask your name
+
+        userInput = Console.ReadLine() 'Reads user input on next line
+
+        If userInput = "Joe" Then
+            Console.WriteLine("Hello! " & userInput)
+        ElseIf userInput = "Emily" Then
+            Console.WriteLine("Hey " & userInput)
+        ElseIf userInput = "Malachi" Then
+            Console.WriteLine("Papa cap?!?")
+        Else
+            Console.WriteLine("Goodbye.")
+
+        End If
+
+    End Sub
+
+End Module
